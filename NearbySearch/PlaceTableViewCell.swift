47b81ad7fe7d923e8a1a3e8683d413930d570233
Michał Iwański <michalronin@outlook.com>
2020-03-15 18:04:48 +0100

    Add location reading && tableview stub

---
+++ NearbySearch/PlaceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceTableViewCell.swift
+//  NearbySearch
+//
+//  Created by Michal Iwanski on 15/03/2020.
+//  Copyright © 2020 michalronin.com. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NearbySearch/PlacesListViewController.swift
+++ NearbySearch/PlacesListViewController.swift
@@ -7,10 +7,73 @@
 //
 
 import UIKit
+import CoreLocation
 
 class PlacesListViewController: UIViewController {
-
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocation?
+    var tableView: UITableView?
+    
+    var places: [Place] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.requestLocation()
+        currentLocation = locationManager?.location
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func configureViewController() {
+        if #available(iOS 13.0, *) {
+            view.backgroundColor = .systemBackground
+        } else {
+            view.backgroundColor = .white
+        }
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        if #available(iOS 13.0, *) {
+            tableView?.backgroundColor = .systemBackground
+        } else {
+            tableView?.backgroundColor = .white
+        }
+    }
+}
+
+extension PlacesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension PlacesListViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         print("error: \(error.localizedDescription)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager?.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.first != nil {
+            print("location: \(locations.first ?? CLLocation())")
+        }
     }
 }
