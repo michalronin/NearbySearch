eaaf42187cd6d20d7a1c35869299011d344998c2
Michał Iwański <michalronin@outlook.com>
2020-03-15 14:33:36 +0100

    Initial Commit

---
+++ NearbySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearbySearch
+//
+//  Created by Michal Iwanski on 15/03/2020.
+//  Copyright © 2020 michalronin.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
