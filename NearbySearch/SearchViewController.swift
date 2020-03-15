5caef7bd46d67a9f0fb14be7d88fb4b41de2983f
Michał Iwański <michalronin@outlook.com>
2020-03-15 15:18:15 +0100

    Add custom title label

--- NearbySearch/SearchViewController.swift
+++ NearbySearch/SearchViewController.swift
@@ -13,5 +13,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
+        
     }
 }
