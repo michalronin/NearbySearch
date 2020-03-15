02aab9f93b974ef3e8abc3f48230f2148d32f531
Michał Iwański <michalronin@outlook.com>
2020-03-15 18:35:46 +0100

    Layout for tableview cell

--- NearbySearch/PlaceTableViewCell.swift
+++ NearbySearch/PlaceTableViewCell.swift
@@ -11,10 +11,12 @@ import UIKit
 class PlaceTableViewCell: UITableViewCell {
     static let reuseID = "PlaceCell"
     let nameLabel = TitleLabel(textAlignment: .left, fontSize: 16)
+    let openNowLabel = BodyLabel(textAlignment: .left)
+    let ratingLabel = BodyLabel(textAlignment: .left)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        configure()
+        configureNameLabel()
     }
     
     func set(place: Place) {
@@ -25,8 +27,37 @@ class PlaceTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configure() {
+    private func configureNameLabel() {
+        addSubview(nameLabel)
         
+        let padding: CGFloat = 8
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            nameLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    private func configureStackView() {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 16
+        
+        stackView.addArrangedSubview(openNowLabel)
+        stackView.addArrangedSubview(ratingLabel)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 8
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            stackView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: padding),
+            stackView.widthAnchor.constraint(equalTo: widthAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 }
 
