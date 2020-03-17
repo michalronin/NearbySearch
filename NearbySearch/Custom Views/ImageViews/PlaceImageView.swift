8e7b77f56b034495de0925bbdf4e7c8203883b35
Michał Iwański <michalronin@outlook.com>
2020-03-17 10:33:30 +0100

    Add image placeholder && custom image view stub

---
+++ NearbySearch/Custom Views/ImageViews/PlaceImageView.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceImageView.swift
+//  NearbySearch
+//
+//  Created by Michal Iwanski on 17/03/2020.
+//  Copyright © 2020 michalronin.com. All rights reserved.
+//
+
+import UIKit
+
+class PlaceImageView: UIImageView {
+    
+}
