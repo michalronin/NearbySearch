7c2054c815e27cd3de3e9538153ec90f7c00f252
Michał Iwański <michalronin@outlook.com>
2020-03-19 10:34:50 +0100

    Add unit test target

---
+++ NearbySearchTests/NearbySearchTests.swift
@@ -0,0 +1,20 @@
+//
+//  NearbySearchTests.swift
+//  NearbySearchTests
+//
+//  Created by Michal Iwanski on 19/03/2020.
+//  Copyright © 2020 michalronin.com. All rights reserved.
+//
+
+import XCTest
+
+class NearbySearchTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+}
